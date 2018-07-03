f8eed195a392c1a1777c0cd7944ecd1dff8adaef
Abey Onalaja <abey.onalaja@gmail.com>
2018-07-03 17:39:16 +0100

    Add Request module

---
+++ src/Session/Request.elm
@@ -0,0 +1,33 @@
+module Session.Request exposing (login)
+
+import Session.AuthToken as AuthToken exposing (AuthToken)
+import User.Model as User exposing (User)
+import Session.Model as Session exposing (Session)
+import Http
+import Json.decode as Decode exposing (Decoder)
+import Json.Encode as Encode
+import Helpers.Request exposing (apiUrl)
+import Util exposing (( => ))
+
+
+login : {r | email : String, password : String } -> Http.Request Session
+login { email , password} =
+    let
+        user =
+            Encode.object
+                [ "email" => Encode.string email
+                , "password" => Encode.string password
+                ]
+        body =
+            user
+                |> Http.jsonBody
+    in
+        decodeSessionResponse
+            |> Http.post (apiUrl "/sessions") body
+
+decodeSessionResponse : Decoder Session
+decodeSessionResponse =
+    Decode.map2 Session
+        (Decode.field "data" User.decoder)
+            (Decode.at ["meta", "token"] AuthToken.decoder)
+
